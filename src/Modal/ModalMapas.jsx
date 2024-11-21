44ac0a64b61c001af971a22fb526d30a592e6530
= <pumaronal2016@gmail.com>
2024-11-21 11:12:44 -0400

    Se agrego cambios de capas y datos en el modal

--- src/Modal/ModalMapas.jsx
+++ src/Modal/ModalMapas.jsx
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaHome } from "react-icons/fa";
+import { RxCaretRight } from "react-icons/rx";
+import { IoLayers } from "react-icons/io5";
+import { FaCalendarAlt } from "react-icons/fa";
+import { TbCalendarWeek } from "react-icons/tb";
+import { TbMapSearch } from "react-icons/tb";
 
-const ModalMapas = ({ open, onClose }) => {
+const estilosMapbox = {
+  topografico: "mapbox://styles/mapbox/outdoors-v12",
+  luciernaga: "mapbox://styles/mapbox/light-v11",
+  calles: "mapbox://styles/mapbox/streets-v12",
+  satelite: "mapbox://styles/mapbox/satellite-v9",
+  dark:"mapbox://styles/mapbox/dark-v11"
+};
+
+const ModalMapas = ({ open, onClose, cambiarEstilo }) => {
+  const [showSubMenu1, setShowSubMenu1] = useState(false);
+  const [showSubMenu2, setShowSubMenu2] = useState(false);
+  const [showSubMenu3, setShowSubMenu3] = useState(false);
+
   if (!open) return null;
 
   return (
@@ -9,55 +27,294 @@ const ModalMapas = ({ open, onClose }) => {
       <div className="fixed inset-0  z-10 pointer-events-none" />
 
       {/* Contenedor del modal interactivo */}
-      <form
-        className="fixed top-4 right-4 max-w-lg w-11/12 max-h-[90vh] bg-white shadow-2xl rounded-2xl p-5 z-20"
-      >
+      <section className="fixed top-4 right-4 w-[35%] max-h-[90vh] bg-black/70 shadow-2xl rounded-2xl z-20">
         {/* Botón para cerrar */}
-        <div className="flex justify-end">
-          <button
-            className="bg-red-500 hover:bg-red-700 px-5 py-1 rounded-md font-bold text-white"
-            onClick={onClose}
-          >
-            X
+        <div className="flex justify-end rounded-tl-2xl rounded-tr-2xl bg-black py-2">
+          <div className="w-[90%]">
+            <h2 className="text-3xl font-bold font-geist text-white text-center">
+              Modo Personalizado
+            </h2>
+          </div>
+          <div className="w-[10%]">
+            <button
+              className=" hover:bg-red-700 px-5 py-1 rounded-md font-bold text-white"
+              onClick={onClose}
+            >
+              X
+            </button>
+          </div>
+        </div>
+
+        <div className="flex justify-around gap-5 mt-5">
+          <button className="bg-gray-500 hover:bg-gray-700 rounded-full font-geist px-5 ">
+            Hoy
           </button>
+          <button className="bg-gray-500 hover:bg-gray-700 rounded-full font-geist px-3 py-2 ">
+            24HRS
+          </button>
+          <button className="bg-gray-500 hover:bg-gray-700 rounded-full font-geist px-3">
+            7DIAS
+          </button>
+          <FaCalendarAlt className="bg-gray-500 rounded-full text-5xl p-2" />
         </div>
 
         {/* Contenido del modal */}
-        <h2 className="text-3xl font-bold text-center">Agregar Entrega</h2>
-        <div className="mt-5 space-y-4">
-          <div>
-            <h3 className="font-semibold">Nombre Entrega</h3>
-            <input
-              type="text"
-              className="border w-full p-2 rounded-lg mt-1"
-              placeholder="Escribe el nombre de la entrega"
-            />
+
+        <section className="mt-5 ">
+          <div className="mb-3">
+            <button
+              className={`w-full flex items-center justify-between gap-4 py-2 px-4  bg-green-700 hover:bg-green-800 text-gray-400 font-geist font-semibold transition-colors
+                                        `}
+              onClick={(e) => {
+                e.preventDefault();
+                setShowSubMenu1(!showSubMenu1);
+              }}
+            >
+              <span className="flex items-center text-start gap-4">
+                <TbCalendarWeek className="font-geist text-2xl text-white " />{" "}
+                Dia de la Semana
+              </span>
+              <RxCaretRight
+                className={`font-bold mt-1 ${
+                  showSubMenu1 && "rotate-90"
+                } transition-all`}
+              />
+            </button>
+            <ul className={`my-2 ${!showSubMenu1 && "hidden"}`}>
+              <li className="flex">
+                <div
+                  style={{ cursor: "pointer" }}
+                  // onClick={rutaUnidadEducativaConvenio}
+                  className={`py-2 px-6 border-l font-extrabold border-gray-500 ml-6 block relative before:w-3 before:h-3 
+                                                before:absolute before:bg-orange before:rounded-full before:-left-[6.5px] before:top-1/2
+                                                before:-translate-y-1/2 before:border-4 before:border-black hover:text-white transition-colors
+                                               `}
+                >
+                  Lu
+                </div>
+                <div
+                  style={{ cursor: "pointer" }}
+                  // onClick={rutaUnidadEducativaConvenio}
+                  className={`py-2 px-6 border-l font-extrabold border-gray-500 ml-6 block relative before:w-3 before:h-3 
+                                                before:absolute before:bg-orange before:rounded-full before:-left-[6.5px] before:top-1/2
+                                                before:-translate-y-1/2 before:border-4 before:border-black hover:text-white transition-colors
+                                               `}
+                >
+                  Ma
+                </div>
+                <div
+                  style={{ cursor: "pointer" }}
+                  // onClick={rutaUnidadEducativaConvenio}
+                  className={`py-2 px-6 border-l font-extrabold border-gray-500 ml-6 block relative before:w-3 before:h-3 
+                                                before:absolute before:bg-orange before:rounded-full before:-left-[6.5px] before:top-1/2
+                                                before:-translate-y-1/2 before:border-4 before:border-black hover:text-white transition-colors
+                                               `}
+                >
+                  Mi
+                </div>
+                <div
+                  style={{ cursor: "pointer" }}
+                  // onClick={rutaUnidadEducativaConvenio}
+                  className={`py-2 px-6 border-l font-extrabold border-gray-500 ml-6 block relative before:w-3 before:h-3 
+                                                before:absolute before:bg-orange before:rounded-full before:-left-[6.5px] before:top-1/2
+                                                before:-translate-y-1/2 before:border-4 before:border-black hover:text-white transition-colors
+                                               `}
+                >
+                  Ju
+                </div>
+                <div
+                  style={{ cursor: "pointer" }}
+                  // onClick={rutaUnidadEducativaConvenio}
+                  className={`py-2 px-6 border-l font-extrabold border-gray-500 ml-6 block relative before:w-3 before:h-3 
+                                                before:absolute before:bg-orange before:rounded-full before:-left-[6.5px] before:top-1/2
+                                                before:-translate-y-1/2 before:border-4 before:border-black hover:text-white transition-colors
+                                               `}
+                >
+                  Vi
+                </div>
+                <div
+                  style={{ cursor: "pointer" }}
+                  // onClick={rutaUnidadEducativaConvenio}
+                  className={`py-2 px-6 border-l font-extrabold border-gray-500 ml-6 block relative before:w-3 before:h-3 
+                                                before:absolute before:bg-orange before:rounded-full before:-left-[6.5px] before:top-1/2
+                                                before:-translate-y-1/2 before:border-4 before:border-black hover:text-white transition-colors
+                                               `}
+                >
+                  Sa
+                </div>
+                <div
+                  style={{ cursor: "pointer" }}
+                  // onClick={rutaUnidadEducativaConvenio}
+                  className={`py-2 px-6 border-l font-extrabold border-gray-500 ml-6 block relative before:w-3 before:h-3 
+                                                before:absolute before:bg-orange before:rounded-full before:-left-[6.5px] before:top-1/2
+                                                before:-translate-y-1/2 before:border-4 before:border-black hover:text-white transition-colors
+                                               `}
+                >
+                  Do
+                </div>
+              </li>
+            </ul>
           </div>
-          <div>
-            <h3 className="font-semibold">Nombre Desayuno</h3>
-            <input
-              type="text"
-              className="border w-full p-2 rounded-lg mt-1"
-              placeholder="Escribe el nombre del desayuno"
-            />
+        </section>
+
+        <section className="mt-3 ">
+          <div className="mb-3">
+            <button
+              className={`w-full flex items-center justify-between gap-4 py-2 px-4  bg-green-700 hover:bg-green-800 text-gray-400 font-geist font-semibold transition-colors
+                                        `}
+              onClick={(e) => {
+                e.preventDefault();
+                setShowSubMenu2(!showSubMenu2);
+              }}
+            >
+              <span className="flex items-center text-start gap-4">
+                <TbMapSearch  className="font-geist text-2xl text-white " />{" "}
+                Análisis por Región
+              </span>
+              <RxCaretRight
+                className={`font-bold mt-1 ${
+                  showSubMenu2 && "rotate-90"
+                } transition-all`}
+              />
+            </button>
+            <ul className={`my-2 ${!showSubMenu2 && "hidden"}`}>
+              <li className="flex">
+                <div
+                  style={{ cursor: "pointer" }}
+                  // onClick={rutaUnidadEducativaConvenio}
+                  className={`py-2 px-6 border-l font-extrabold border-gray-500 ml-6 block relative before:w-3 before:h-3 
+                                                before:absolute before:bg-orange before:rounded-full before:-left-[6.5px] before:top-1/2
+                                                before:-translate-y-1/2 before:border-4 before:border-black hover:text-white transition-colors
+                                               `}
+                >
+                  Ninguna
+                </div>
+                <div
+                  style={{ cursor: "pointer" }}
+                  // onClick={rutaUnidadEducativaConvenio}
+                  className={`py-2 px-6 border-l font-extrabold border-gray-500 ml-6 block relative before:w-3 before:h-3 
+                                                before:absolute before:bg-orange before:rounded-full before:-left-[6.5px] before:top-1/2
+                                                before:-translate-y-1/2 before:border-4 before:border-black hover:text-white transition-colors
+                                               `}
+                >
+                  Distrito
+                </div>
+                
+                <div
+                  style={{ cursor: "pointer" }}
+                  // onClick={rutaUnidadEducativaConvenio}
+                  className={`py-2 px-6 border-l font-extrabold border-gray-500 ml-6 block relative before:w-3 before:h-3 
+                                                before:absolute before:bg-orange before:rounded-full before:-left-[6.5px] before:top-1/2
+                                                before:-translate-y-1/2 before:border-4 before:border-black hover:text-white transition-colors
+                                               `}
+                >
+                  Municipio
+                </div>
+                <div
+                  style={{ cursor: "pointer" }}
+                  // onClick={rutaUnidadEducativaConvenio}
+                  className={`py-2 px-6 border-l font-extrabold border-gray-500 ml-6 block relative before:w-3 before:h-3 
+                                                before:absolute before:bg-orange before:rounded-full before:-left-[6.5px] before:top-1/2
+                                                before:-translate-y-1/2 before:border-4 before:border-black hover:text-white transition-colors
+                                               `}
+                >
+                  Provincia
+                </div>
+              </li>
+            </ul>
           </div>
-          <div>
-            <h3 className="font-semibold">Cantidad</h3>
-            <input
-              type="number"
-              className="border w-full p-2 rounded-lg mt-1"
-              placeholder="Escribe la cantidad"
-            />
-          </div>
-          <div>
-            <h3 className="font-semibold">Fecha</h3>
-            <input
-              type="date"
-              className="border w-full p-2 rounded-lg mt-1"
-            />
+        </section>
+
+        <div className="mt-1 ">
+          <div className="mb-3">
+            <button
+              className={`w-full flex items-center justify-between gap-4 py-2 px-4  bg-green-700 hover:bg-green-800 text-gray-400 font-geist font-semibold transition-colors
+                                        `}
+              onClick={(e) => {
+                e.preventDefault();
+                setShowSubMenu3(!showSubMenu3);
+              }}
+            >
+              <span className="flex items-center text-start gap-4">
+                <IoLayers className="font-geist text-2xl text-white" /> Capas
+              </span>
+              <RxCaretRight
+                className={` font-bold mt-1 ${
+                  showSubMenu2 && "rotate-90"
+                } transition-all`}
+              />
+            </button>
+            <ul className={`my-2 ${!showSubMenu3 && "hidden"}`}>
+              <li>
+                <div
+                  style={{ cursor: "pointer" }}
+                  onClick={() => cambiarEstilo("topografico")}
+                  className={`py-2 px-6 border-l font-extrabold border-gray-500 ml-6 block relative before:w-3 before:h-3 
+                                                before:absolute before:bg-orange before:rounded-full before:-left-[6.5px] before:top-1/2
+                                                before:-translate-y-1/2 before:border-4 before:border-black hover:text-white transition-colors
+                                               `}
+                >
+                  topográfico
+                </div>
+              </li>
+
+              <li>
+                <div
+                  style={{ cursor: "pointer" }}
+                  onClick={() => cambiarEstilo("luciernaga")}
+                  className={`py-2 px-6 border-l font-extrabold border-gray-500 ml-6 block relative before:w-3 before:h-3 
+                                                before:absolute before:bg-orange before:rounded-full before:-left-[6.5px] before:top-1/2
+                                                before:-translate-y-1/2 before:border-4 before:border-black hover:text-white transition-colors
+                                                `}
+                >
+                  {/* ${location.pathname === '/unidad/educativa/publica'&& 'bg-primary-900/50 rounded-full text-white'} */}
+                  Luciérnaga
+                </div>
+              </li>
+
+              <li>
+                <div
+                  style={{ cursor: "pointer" }}
+                  onClick={() => cambiarEstilo("calles")}
+                  className={`py-2 px-6 border-l font-extrabold border-gray-500 ml-6 block relative before:w-3 before:h-3 
+                                                before:absolute before:bg-gray-500 before:rounded-full before:-left-[6.5px] before:top-1/2
+                                                before:-translate-y-1/2 before:border-4 before:border-black hover:text-white transition-colors
+                                                `}
+                >
+                  {/* ${location.pathname ==='/unidad/educativa/privada'&& 'bg-primary-900/50 rounded-full text-white' } */}
+                  calles
+                </div>
+              </li>
+              <li>
+                <div
+                  style={{ cursor: "pointer" }}
+                  onClick={() => cambiarEstilo("satelite")}
+                  className={`py-2 px-6 border-l font-extrabold border-gray-500 ml-6 block relative before:w-3 before:h-3 
+                                                before:absolute before:bg-gray-500 before:rounded-full before:-left-[6.5px] before:top-1/2
+                                                before:-translate-y-1/2 before:border-4 before:border-black hover:text-white transition-colors
+                                                `}
+                >
+                  {/* ${location.pathname ==='/unidad/educativa/privada'&& 'bg-primary-900/50 rounded-full text-white' } */}
+                  Satelite
+                </div>
+              </li>
+              <li>
+                <div
+                  style={{ cursor: "pointer" }}
+                  onClick={() => cambiarEstilo("dark")}
+                  className={`py-2 px-6 border-l font-extrabold border-gray-500 ml-6 block relative before:w-3 before:h-3 
+                                                before:absolute before:bg-gray-500 before:rounded-full before:-left-[6.5px] before:top-1/2
+                                                before:-translate-y-1/2 before:border-4 before:border-black hover:text-white transition-colors
+                                                `}
+                >
+                  {/* ${location.pathname ==='/unidad/educativa/privada'&& 'bg-primary-900/50 rounded-full text-white' } */}
+                  Dark
+                </div>
+              </li>
+            </ul>
           </div>
         </div>
-      </form>
+      </section>
     </>
   );
 };
--- src/page/MapaPage.jsx
+++ src/page/MapaPage.jsx
@@ -1,4 +1,3 @@
-
 import { FiAlignJustify } from "react-icons/fi";
 import React, { useState } from "react";
 import { useMapBox } from "../hooks/useMapBox";
@@ -10,32 +9,75 @@ const puntoInicial = {
   zoom: 13.5,
 };
 
-const MapaPage = () => {
-  const { setRef, coords, area, centro } = useMapBox(puntoInicial);
+
+
+
+const MapaPage = (estilo) => {
+  const { setRef, coords, area, centro, mapa } = useMapBox(puntoInicial);
   const [openModal, setOpenModal] = useState(false);
+
+  const zoomIn = () => {
+    if (mapa.current) {
+      mapa.current.zoomIn();
+    }
+  };
+
+  const zoomOut = () => {
+    if (mapa.current) {
+      mapa.current.zoomOut();
+    }
+  };
+
+  // Función para cambiar el estilo del mapa
+  const cambiarEstilo = (estilo) => {
+    if (mapa.current) {
+      const estilosMapbox = {
+        topografico: "mapbox://styles/mapbox/outdoors-v12",
+        luciernaga: "mapbox://styles/mapbox/light-v11",
+        calles: "mapbox://styles/mapbox/streets-v12",
+        satelite: "mapbox://styles/mapbox/satellite-v9",
+        dark:"mapbox://styles/mapbox/dark-v11"
+      };
+      mapa.current.setStyle(estilosMapbox[estilo]);
+    }
+  };
 
   return (
     <>
-      <ModalMapas open={openModal} onClose={() => setOpenModal(false)} />
+      <ModalMapas open={openModal} onClose={() => setOpenModal(false)} cambiarEstilo={cambiarEstilo} />
 
       <div className="info p-4 bg-gray-800 text-white fixed top-4 left-4 rounded-lg shadow-lg z-10">
         Lng: {coords.lng} | Lat: {coords.lat} | Zoom: {coords.zoom} <br />
         Área: {area} km² <br />
-        Centro: {centro ? `${centro[1].toFixed(4)}, ${centro[0].toFixed(4)}` : "N/A"}
+        Centro:{" "}
+        {centro ? `${centro[1].toFixed(4)}, ${centro[0].toFixed(4)}` : "N/A"}
       </div>
 
-      {
-        openModal ? null: (
+      {openModal ? null : (
         <div className="fixed top-4 right-4 z-10">
           <button
-            className="px-4 py-2 bg-blue-500 text-white rounded-lg shadow-lg hover:bg-blue-600 flex items-center justify-center"
+            className="px-4 py-4 bg-blue-500 text-white rounded-full shadow-lg hover:bg-blue-600 flex items-center justify-center"
             onClick={() => setOpenModal(true)}
           >
             <FiAlignJustify size={24} /> {/* Ajusta el tamaño aquí */}
           </button>
         </div>
-        )
-      }
+      )}
+
+      <div className="fixed left-4 top-32 hover:bg-white border-2 border-white rounded-lg z-10 p-2">
+        <button
+          className=" bg-white font-bold text-3xl  hover:bg-orange-600 w-full"
+          onClick={zoomIn}
+        >
+          +
+        </button>
+        <button
+          className="mt-2 bg-white font-bold text-3xl  hover:bg-orange-600 w-full"
+          onClick={zoomOut}
+        >
+          -
+        </button>
+      </div>
 
       <div className="mapContainer h-screen" ref={setRef} />
     </>
